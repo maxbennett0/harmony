f8db6956303ca65978626c3de9804fee1b7a0fbe
Lucas Plummer <lucasplummer1112@gmail.com>
2022-12-23 11:40:09 -0700

    werid branxh

--- harmony.client/src/env.js
+++ harmony.client/src/env.js
@@ -1,6 +1,8 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-tkmccijvrcuc0oxw.us.auth0.com'
+export const clientId = 'VBQk2WRibquQa1oQtpL32FoJMizP6Tu2'
+export const audience = 'https://max-classroom.com'
+
+
