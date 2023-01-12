297585c5215b289cb7f027f34d4b4118487e9362
Max Bennett <maxbennett602@gmail.com>
2023-01-12 16:33:56 -0700

    slider is mostly working, interval issues

--- harmony.client/src/App.vue
+++ harmony.client/src/App.vue
@@ -5,25 +5,31 @@
   <main class="color-background">
     <router-view />
   </main>
-  <footer class="container-fluid sticky-bottom bg-dark p-3">
-    <audio v-if="activeSong" id="player" class="bg-dark" @loadeddata="setSongDuration" hidden :src="activeSong.songUrl">
+  <footer v-if="activeSong" class="container-fluid d-flex sticky-bottom bg-dark p-3">
+    <audio id="player" class="bg-dark" @loadeddata="setSongDuration" hidden :src="activeSong.songUrl">
     </audio>
-    <i v-if="activeSong" id="shuffleButton" class="mdi mdi-shuffle text-white selectable"
-      @click="findShuffleSong(activeSong.id)"></i>
-    <i v-if="activeSong" id="previousButton" class="mdi mdi-skip-previous text-white selectable"
-      @click="findPreviousSong(activeSong.id)"></i>
-    <i v-if="activeSong" id="playButton" class="mdi mdi-play text-white selectable" @click="playSong"></i>
-    <i v-if="activeSong" id="pauseButton" class="mdi mdi-pause text-white selectable" @click="pauseSong"></i>
-    <i v-if="activeSong" id="nextButton" class="mdi mdi-skip-next text-white selectable"
-      @click="findNextSong(activeSong.id)"></i>
-    <h3 v-if="activeSong">
-      <span id="currentTime">{{ songDuration }}</span>
+    <div class="icons">
+      <i id="shuffleButton" class="mdi mdi-shuffle text-white selectable" @click="findShuffleSong(activeSong.id)"></i>
+      <i id="previousButton" class="mdi mdi-skip-previous text-white selectable"
+        @click="findPreviousSong(activeSong.id)"></i>
+      <i v-if="activeSong" id="pauseButton" class="mdi mdi-pause text-white selectable" @click="pauseSong"></i>
+      <i id="playButton" class="mdi mdi-play text-white selectable" @click="playSong"></i>
+      <i id="nextButton" class="mdi mdi-skip-next text-white selectable" @click="findNextSong(activeSong.id)"></i>
+    </div>
+    <h3 id="currentTime" class="align-items-center d-flex mx-2">
+      <span>{{ formatTime(currentTime) }}</span>
     </h3>
-    <div v-if="activeSong" class="progress text-dark">
-      <div preload="metadata" class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="25"
-        aria-valuemin="0" :aria-valuemax=songDuration>
+    <div class="d-flex align-items-center bar-length">
+      <div class="progress text-dark">
+        <div preload="metadata" class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="25"
+          aria-valuemin="0" :aria-valuemax=songDuration>
+        </div>
+        <input id="seekSlider" preload="metadata" type="range" min="0" :max="songDuration" v-model="currentTime"
+          @mouseup="setSongTime" @mousedown="seeking = true">
       </div>
-      <input id="seekSlider" preload="metadata" type="range" min="0" :max=songDuration>
+      <h3 id="endingTime" class="align-items-center d-flex mx-2">
+        {{ formatTime(songDuration) }}
+      </h3>
     </div>
   </footer>
 </template>
@@ -39,37 +45,75 @@ import Pop from "./utils/Pop.js"
 
 export default {
   setup() {
+    const currentTime = ref(0)
     const songDuration = ref(0)
+    const playState = ref(false)
+    const seeking = ref(false)
+    let playInterval = null
+    const autoPlay = ref(false)
     const route = useRoute()
+    function incrementPlayer() {
+      if (!seeking.value) currentTime.value += 1
+      if (currentTime.value >= songDuration.value) {
+        pauseSong()
+        currentTime.value = 0
+        if (autoPlay.value) {
+          nextSong()
+          playSong()
+        }
+      }
+    }
+    function playSong() {
+      const player = document.getElementById('player')
+      playState.value = true
+      if (!playInterval)
+        playInterval = setInterval(incrementPlayer, 1000)
+      player?.play()
+    }
+    function pauseSong() {
+      playState.value = false
+      playInterval = clearInterval(playInterval)
+      player.pause()
+    }
+    function nextSong(id) {
+      const songs = AppState.songs
+      let foundIndex = songs.findIndex(s => s.id == id)
+      let nextSongIndex = foundIndex + 1
+      let nextSong = songs[nextSongIndex]
+      return nextSong
+    }
+    function previousSong(id) {
+      const songs = AppState.songs
+      let foundIndex = songs.findIndex(s => s.id == id)
+      let nextSongIndex = foundIndex - 1
+      let nextSong = songs[nextSongIndex]
+      return nextSong
+    }
     return {
+      seeking,
+      currentTime,
       songDuration,
       route,
       appState: computed(() => AppState),
       songs: computed(() => AppState.songs),
       activeSong: computed(() => AppState.activeSong),
+      playSong,
+      pauseSong,
+      nextSong,
+      previousSong,
 
-      playSong() {
+      setSongTime(ev) {
+        logger.log(ev.target.value)
+        let set = parseInt(ev.target.value)
         const player = document.getElementById('player')
-        player?.play()
-      },
-      pauseSong() {
-        player.pause()
+        player.currentTime = set
+        currentTime.value = set
+        seeking.value = false
+        if (!playState.value) {
+          playSong()
+        }
       },
       // NOTE find song by the id. with id, find index, play song with at new index
-      nextSong(id) {
-        const songs = AppState.songs
-        let foundIndex = songs.findIndex(s => s.id == id)
-        let nextSongIndex = foundIndex + 1
-        let nextSong = songs[nextSongIndex]
-        return nextSong
-      },
-      previousSong(id) {
-        const songs = AppState.songs
-        let foundIndex = songs.findIndex(s => s.id == id)
-        let nextSongIndex = foundIndex - 1
-        let nextSong = songs[nextSongIndex]
-        return nextSong
-      },
       shuffleSong() {
         const songs = AppState.songs
         let shuffleIndex = Math.floor(Math.random() * (songs.length - 1))
@@ -78,7 +122,7 @@ export default {
         return shuffled
       },
       async findNextSong(songId) {
-        let nextSong = this.nextSong(songId)
+        let nextSong = nextSong(songId)
         logger.log(nextSong)
         try {
           await songsService.findSongById(nextSong.id)
@@ -89,7 +133,7 @@ export default {
         }
       },
       async findPreviousSong(songId) {
-        let previousSong = this.previousSong(songId)
+        let previousSong = previousSong(songId)
         logger.log(previousSong)
         try {
           await songsService.findSongById(previousSong.id)
@@ -112,10 +156,15 @@ export default {
       },
       setSongDuration(ev) {
         let audioTag = ev.target
-        let songTime = songDuration.value = (audioTag.duration * 0.0166).toFixed(2)
-        const secDuration = Math.floor((songTime /= 1000) % 60)
-        return secDuration
+        let duration = audioTag.duration
+        songDuration.value = duration
       },
+      formatTime(s) {
+        let minutes = Math.floor(s / 60)
+        let seconds = Math.trunc(s % 60)
+        seconds = seconds < 10 ? '0' + seconds : seconds
+        return minutes + ':' + seconds
+      }
     }
   },
   components: { Navbar }
@@ -127,6 +176,10 @@ export default {
 
 #grad {
   background-image: linear-gradient(to bottom right, red, yellow);
+}
+
+.icons {
+  width: 20%;
 }
 
 
@@ -167,7 +220,32 @@ footer {
   font-size: xx-large;
 }
 
-#progress {
-  width: 85%;
+#seekSlider {
+  width: 100%;
+}
+
+#currentTime {
+  margin: 0%;
+}
+
+#endingTime {
+  margin: 0%;
+}
+
+.bar-length {
+  width: 100%;
+}
+
+.progress {
+  width: 100%;
+}
+
+.progress-bar {
+  width: 100%;
+}
+
+#currentTime {
+  margin: 0px;
+  padding: 0%;
 }
 </style>
